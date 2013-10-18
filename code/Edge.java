16a4f7f57db42fa1f5b15aecdb5ff44af29a9916
Andres <tuulemaa.dyndns.org>
2013-10-18 12:26:52 +0300

    initial version, application model.

---
+++ code/Edge.java
@@ -0,0 +1,12 @@
+/**
+ * @(#) Edge.java
+ */
+
+public class Edge
+{
+	private int on_critical_path;
+	
+	private Node in_edge;
+	
+	
+}
---
+++ code/Node.java
@@ -0,0 +1,16 @@
+/**
+ * @(#) Node.java
+ */
+
+public class Node
+{
+	private int name;
+	
+	private int duration;
+	
+	private int on_critical_path;
+	
+	private Edge in_node;
+	
+	
+}
